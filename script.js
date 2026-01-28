23f78927ae9405bfa379ed371e9b4a607e12a15d
Michel <221404137+It-is-Michel@users.noreply.github.com>
2026-01-28 19:55:15 -0300

    chore: Add a function to get the user choice

--- script.js
+++ script.js
@@ -7,3 +7,21 @@ function getComputerChoice() {
         case 2: return "scissors";
     }
 }
+
+function getHumanChoice() {
+    let humanChoice = prompt("Choose 'rock', 'paper' or 'scissors' by writing it or using a number from 1 to 3", "rock")
+
+    switch (humanChoice.toLocaleLowerCase()) {
+        case "rock":
+        case "1":
+            return "rock";
+            
+        case "paper":
+        case "2":
+            return "paper";
+
+        case "scissors":
+        case "3":
+            return "scissors";
+    }
+}
