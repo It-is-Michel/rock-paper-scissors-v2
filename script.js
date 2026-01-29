1ad269d5f1e2218faa519e5758b393850e2ab188
Michel <221404137+It-is-Michel@users.noreply.github.com>
2026-01-29 18:20:23 -0300

    chore: Stop getHumanChoice() from taking numbers from the user

--- script.js
+++ script.js
@@ -9,23 +9,9 @@ function getComputerChoice() {
 }
 
 function getHumanChoice() {
-    let humanChoice = prompt("Choose 'rock', 'paper' or 'scissors' by writing it or using a number from 1 to 3", "rock")
-
-    switch (humanChoice.toLocaleLowerCase()) {
-        case "rock":
-        case "1":
-            return "rock";
-            
-        case "paper":
-        case "2":
-            return "paper";
-
-        case "scissors":
-        case "3":
-            return "scissors";
-        
-        default: return "rock";
-    }
+    let humanChoice = prompt("Choose 'rock', 'paper' or 'scissors'", "rock")
+    
+    return humanChoice;
 }
 
 let humanScore = 0;
